a133c59e5db0bda2295248f109bc32ead0e0af89
Ziqian Wei <68311861+weiziqianpsych@users.noreply.github.com>
2023-07-17 10:23:41 +0800

    Update conf.py

--- cookiemilk_tutorial/source/conf.py
+++ cookiemilk_tutorial/source/conf.py
@@ -9,7 +9,7 @@
 project = 'cookiemilk'
 copyright = '2023, weiziqian'
 author = 'weiziqian'
-release = '0.01'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
